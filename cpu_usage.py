c6725a2fcbb78f6fb7820d46c6a7a810cfd8d4ab
ramses2099 <ramses2099@gmail.com>
2023-05-17 18:19:27 -0400

    new commit

---
+++ cpu_usage.py
@@ -0,0 +1,16 @@
+##!/usr/bin/env python3
+
+import psutil
+
+
+def check_cpu_usage(percent):
+    usage = psutil.cpu_percent(1)
+    print("DEBUG: usage: {}%".format(usage))
+    return usage < percent
+
+
+if __name__ == "__main__":
+    if not check_cpu_usage(75):
+        print("ERROR! CPU is overloaded")
+    else:
+        print("Everything ok")
